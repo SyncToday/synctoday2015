f467ae24826e750cc7d5dcbc45694fb4a4fd8e47
davidpodhola <david.podhola@naseukoly.cz>
2014-12-17 23:21:21 +0100

    Microsoft Dynamics CRM Companies

---
+++ Microsoft Dynamics CRM Companies/Program.fs
@@ -0,0 +1,20 @@
+﻿module Program
+
+open System
+open FSharpx.TypeProviders.XrmProvider
+
+let xrm = XrmDataProvider<"https://", Username="", Password="">.GetDataContext("https://.../XRMServices/2011/Organization.svc", "username", "password", "server")
+
+[<EntryPoint>]
+let main argv = 
+    let accounts = xrm.accountSet |> Seq.toList
+    let activeAccounts = query {
+                            for account in accounts do
+                            where ( ( int account.statecode )  = 0 )
+                            select account
+                        }                        
+    for account in activeAccounts do
+        printfn "%A" account.name
+
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
