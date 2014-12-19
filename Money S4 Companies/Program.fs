dcde7c971a08fb2827acb1efc14baf9fb0216788
davidpodhola <david.podhola@naseukoly.cz>
2014-12-19 06:38:29 +0100

    writing Money S4 result to a file

--- Money S4 Companies/Program.fs
+++ Money S4 Companies/Program.fs
@@ -1,6 +1,10 @@
 ﻿module Program
 
+open System.IO
+open System.Text
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" MoneyS4.output
+    let result = [| MoneyS4.output.ToString() |]
+    File.WriteAllLines( argv.[0], result, Encoding.UTF8 )     
     0 // return an integer exit code
