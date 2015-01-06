49fd80442f48a24341a427919f8162fc3437c233
davidpodhola <david.podhola@naseukoly.cz>
2015-01-06 06:14:24 +0100

    Geis work in progress

--- Geis Expedice/GeisExpedice.fs
+++ Geis Expedice/GeisExpedice.fs
@@ -29,14 +29,6 @@ type ExpediceXml = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?>
 <sendContactEmail></sendContactEmail>
 <sendContactPhone></sendContactPhone>
 <usePrintSend>0</usePrintSend>
-<printSendName></printSendName>
-<printSendStreet></printSendStreet>
-<printSendStreetNumOri></printSendStreetNumOri>
-<printSendStreetNumDesc></printSendStreetNumDesc>
-<printSendCity></printSendCity>
-<printSendZipCode></printSendZipCode>
-<printSendCountry></printSendCountry>
-<printSendContactPhone></printSendContactPhone>
 <recContactName></recContactName>
 <recName>Geis</recName>
 <recStreet>Zemská</recStreet>
@@ -50,20 +42,6 @@ type ExpediceXml = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?>
 <addrCode>60046672</addrCode>
 <recContactPhone></recContactPhone>
 <recContactEmail></recContactEmail>
-<pasName></pasName>
-<pickUpDate>7.1.2015</pickUpDate>
-<pickUpMinutesFrom></pickUpMinutesFrom>
-<pickUpMinutesTo></pickUpMinutesTo>
-<routingCodeInput></routingCodeInput>
-<routingCodeOutput></routingCodeOutput>
-<backInfoEmail></backInfoEmail>
-<services>
-<COD>
-<codValue>1000</codValue>
-<codValueCur>CZK</codValueCur>
-<varCode>123456789</varCode>
-</COD>
-</services>
 <rows>
 <row>
 <note></note>
@@ -95,14 +73,6 @@ type ExpediceXml = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?>
 <sendContactEmail></sendContactEmail>
 <sendContactPhone></sendContactPhone>
 <usePrintSend>0</usePrintSend>
-<printSendName></printSendName>
-<printSendStreet></printSendStreet>
-<printSendStreetNumOri></printSendStreetNumOri>
-<printSendStreetNumDesc></printSendStreetNumDesc>
-<printSendCity></printSendCity>
-<printSendZipCode></printSendZipCode>
-<printSendCountry></printSendCountry>
-<printSendContactPhone></printSendContactPhone>
 <recContactName></recContactName>
 <recName>Geis</recName>
 <recStreet>Zemská</recStreet>
@@ -116,20 +86,6 @@ type ExpediceXml = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?>
 <addrCode>60046672</addrCode>
 <recContactPhone></recContactPhone>
 <recContactEmail></recContactEmail>
-<pasName></pasName>
-<pickUpDate>7.1.2015</pickUpDate>
-<pickUpMinutesFrom></pickUpMinutesFrom>
-<pickUpMinutesTo></pickUpMinutesTo>
-<routingCodeInput></routingCodeInput>
-<routingCodeOutput></routingCodeOutput>
-<backInfoEmail></backInfoEmail>
-<services>
-<COD>
-<codValue>1000</codValue>
-<codValueCur>CZK</codValueCur>
-<varCode>123456789</varCode>
-</COD>
-</services>
 <rows>
 <row>
 <note></note>
@@ -161,14 +117,6 @@ type ExpediceXml = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?>
 <sendContactEmail></sendContactEmail>
 <sendContactPhone></sendContactPhone>
 <usePrintSend>0</usePrintSend>
-<printSendName></printSendName>
-<printSendStreet></printSendStreet>
-<printSendStreetNumOri></printSendStreetNumOri>
-<printSendStreetNumDesc></printSendStreetNumDesc>
-<printSendCity></printSendCity>
-<printSendZipCode></printSendZipCode>
-<printSendCountry></printSendCountry>
-<printSendContactPhone></printSendContactPhone>
 <recContactName>Pavel Novák</recContactName>
 <recName>Geis</recName>
 <recStreet>Zemská</recStreet>
@@ -182,20 +130,6 @@ type ExpediceXml = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?>
 <addrCode>60046672</addrCode>
 <recContactPhone>+420777666555</recContactPhone>
 <recContactEmail></recContactEmail>
-<pasName></pasName>
-<pickUpDate>8.1.2015</pickUpDate>
-<pickUpMinutesFrom></pickUpMinutesFrom>
-<pickUpMinutesTo></pickUpMinutesTo>
-<routingCodeInput></routingCodeInput>
-<routingCodeOutput></routingCodeOutput>
-<backInfoEmail></backInfoEmail>
-<services>
-<COD>
-<codValue>1000</codValue>
-<codValueCur>CZK</codValueCur>
-<varCode>123456789</varCode>
-</COD>
-</services>
 <rows>
 <row>
 <note></note>
@@ -227,14 +161,6 @@ type ExpediceXml = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?>
 <sendContactEmail></sendContactEmail>
 <sendContactPhone></sendContactPhone>
 <usePrintSend>0</usePrintSend>
-<printSendName></printSendName>
-<printSendStreet></printSendStreet>
-<printSendStreetNumOri></printSendStreetNumOri>
-<printSendStreetNumDesc></printSendStreetNumDesc>
-<printSendCity></printSendCity>
-<printSendZipCode></printSendZipCode>
-<printSendCountry></printSendCountry>
-<printSendContactPhone></printSendContactPhone>
 <recContactName>Pavel Novák</recContactName>
 <recName></recName>
 <recStreet></recStreet>
@@ -248,20 +174,6 @@ type ExpediceXml = XmlProvider<"""<?xml version="1.0" encoding="utf-8"?>
 <addrCode>60046672</addrCode>
 <recContactPhone>+420777666555</recContactPhone>
 <recContactEmail></recContactEmail>
-<pasName></pasName>
-<pickUpDate>13.1.2015</pickUpDate>
-<pickUpMinutesFrom></pickUpMinutesFrom>
-<pickUpMinutesTo></pickUpMinutesTo>
-<routingCodeInput></routingCodeInput>
-<routingCodeOutput></routingCodeOutput>
-<backInfoEmail></backInfoEmail>
-<services>
-<COD>
-<codValue>1000</codValue>
-<codValueCur>CZK</codValueCur>
-<varCode>123456789</varCode>
-</COD>
-</services>
 <rows>
 <row>
 <note></note>
@@ -292,6 +204,9 @@ let private fullContextMoney = contextMoney.DataContext
 
 let private activeExpeditions = query {
                         for expedition in context.adapters_geis_Expeditions do
+                        where (
+                            expedition.ExportId = Nullable<Guid>()
+                        ) 
                         select expedition
                     }                        
 
@@ -302,4 +217,10 @@ let Fakturace_FakturaVydana =  query {
 
 let output =
     ExpediceXml.ArrayOfPackage [| for expedition in activeExpeditions do
-                                    yield ExpediceXml.Package() |]
+                                    yield ExpediceXml.Package( isCargo(expedition), 0, number(expedition), customerReference(expedition ), documentNumber(expedition),
+                                                                null, null, null, null, sendName(expedition), sendStreet(expedition), sendStreetNumOri(expedition),
+                                                                sendStreetNumDesc(expedition), sendCity(expedition), sentZipCode(expedition), sendCountry(expedition), sendContactName(expedition),
+                                                                sendContactEmail(expedition), sendContactPhone(expedition), 0, recContactName(expedition), recName(expedition), recStreet(expedition), recStreetNumOri(expedition), 
+                                                                recStreetNumDesc(expedition), recCity(expedition), recZipCode(expedition), recCountry(expedition), recNote(expedition), driverNote(expedition), addrCode(expedition),
+                                                                recContactPhone(expedition), recContactEmail(expedition), rows(expedition)
+                                                                  ) |]
