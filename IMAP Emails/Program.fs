8be9a8beaf270cb0b8f672e7d0b9e437604e2b16
davidpodhola <david.podhola@naseukoly.cz>
2015-01-09 09:35:14 +0100

    IMAP Emails started

---
+++ IMAP Emails/Program.fs
@@ -0,0 +1,14 @@
+﻿open S22.Imap
+let mutable client = null
+
+[<EntryPoint>]
+let main argv = 
+    //printfn "%A" argv
+    client <- new ImapClient("imap.gmail.com", 993, 
+      "@gmail.com", "", AuthMethod.Login, true)    
+    let uids = client.Search( SearchCondition.All() )
+    for uid in uids do
+        printfn "%A" uid
+        let message = client.GetMessage(uid)
+        printfn "%A" message.From 
+    0 // return an integer exit code
