a9c9de8a3b8d6d271f60b13b91af94abe9e65448
davidpodhola <david.podhola@naseukoly.cz>
2015-01-25 15:54:45 +0100

    Workflow sample

---
+++ ActivityLibrary1/SendEmail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+
+namespace ActivityLibrary1
+{
+    // http://www.codeproject.com/Tips/640802/Create-SendEmail-Workflow-Custom-Activity
+    public sealed class SendEmailActivity : CodeActivity
+    {
+        public InArgument<string> from { get; set; }
+        public InArgument<string> host { get; set; }
+        public InArgument<string> userName { get; set; }
+        public InArgument<string> password { get; set; }
+        public InArgument<string> to { get; set; }
+        public InArgument<string> subject { get; set; }
+        public InArgument<string> body { get; set; }
+        public OutArgument<string> result { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            var mailMessage = new System.Net.Mail.MailMessage();
+            mailMessage.To.Add(to.Get(context).ToString());
+            mailMessage.Subject = subject.Get(context).ToString();
+            mailMessage.Body = body.Get(context);
+            mailMessage.From =
+            new System.Net.Mail.MailAddress(from.Get(context));
+            var smtp = new System.Net.Mail.SmtpClient();
+            smtp.Host = host.Get(context);
+            smtp.Credentials =
+            new System.Net.NetworkCredential(
+            userName.Get(context), password.Get(context));
+            smtp.EnableSsl = true;
+            smtp.Send(mailMessage);
+            result.Set(context, "Sent Email Successfully!");
+        }
+    }
+}
