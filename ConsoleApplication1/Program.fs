df13d3a0b660aa4b24a444e2c10bd3ae5ce4c8a2
davidpodhola <david.podhola@naseukoly.cz>
2015-01-25 16:20:55 +0100

    comment added

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -7,6 +7,7 @@ open System.Activities.XamlIntegration
 
 [<EntryPoint>]
 let main argv = 
+    // http://stackoverflow.com/questions/14229129/workflow-foundation-4-5-expression-activity-type-csharpvalue1-requires-compi
     let settings = ActivityXamlServicesSettings( CompileExpressions = true )
     let xaml = File.ReadAllText("Workflow1.xaml")
     let reader = new StringReader(xaml)
