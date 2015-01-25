8655c034279ee7b1e8d8fdba85ccfd1b65b57488
davidpodhola <david.podhola@naseukoly.cz>
2015-01-25 16:04:43 +0100

    sample workflow + usage

---
+++ ActivityLibrary1/LastSync.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+
+namespace ActivityLibrary1
+{
+    public sealed class LastSync : CodeActivity
+    {
+        public OutArgument<DateTime> result { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            result.Set(context, DateTime.Now.AddHours(-1.0));
+        }
+
+    }
+}
