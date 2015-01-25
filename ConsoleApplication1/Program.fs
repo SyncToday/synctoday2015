f70af11877174545ce374dee10340ad001f7ed88
davidpodhola <david.podhola@naseukoly.cz>
2015-01-25 16:20:28 +0100

    ConsoleApplication1 runs XAML workflow in F#

---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.IO
+open System.Linq
+open System.Activities
+open System.Activities.Statements
+open System.Activities.XamlIntegration
+
+[<EntryPoint>]
+let main argv = 
+    let settings = ActivityXamlServicesSettings( CompileExpressions = true )
+    let xaml = File.ReadAllText("Workflow1.xaml")
+    let reader = new StringReader(xaml)
+    let activity = ActivityXamlServices.Load(reader, settings)
+    WorkflowInvoker.Invoke(activity) |> ignore
+    0 // return an integer exit code
