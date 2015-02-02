a9e4d5cf4248067c16fe588d2033775dbcb8fbfc
David Podhola <david.podhola@naseukoly.cz>
2015-02-02 02:55:39 +0000

    sync.today.server started
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C32

---
+++ sync.today.server/Program.fs
@@ -0,0 +1,14 @@
+﻿open Microsoft.Owin.Hosting
+open System
+open System.Net.Http
+
+[<EntryPoint>]
+let main argv = 
+    let baseAddress = "http://localhost:8000"
+    let server = WebApp.Start<sync.today.Startup>(baseAddress)
+    //really dumb way to lock console, 
+    //server should be wraped in windows service 
+    Console.WriteLine("Press enter to exit ... ")
+    Console.ReadLine() |> ignore
+    server.Dispose() 
+    0 
