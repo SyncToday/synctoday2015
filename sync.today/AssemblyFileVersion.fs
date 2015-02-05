154d61472ae048b81d77fb17893321b0595200c8
David Podhola <david.podhola@naseukoly.cz>
2015-02-05 21:28:03 +0000

    Index.html copied to the working dir (should it?)
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C39

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.05.2152")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.05.2221")>]
       ()
