354ec4f1a67e4624ac0f40f5a30f152d49e6e98c
David Podhola <david.podhola@naseukoly.cz>
2015-02-05 20:11:18 +0000

    sync.today.service started
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C35

---
+++ sync.today.service/Installer.fs
@@ -0,0 +1,35 @@
+﻿namespace sync.today.service
+
+open System
+open System.Configuration
+open System.Configuration.Install
+open System.ComponentModel
+open System.Linq
+open System.ServiceProcess
+
+[<RunInstaller(true)>]
+type public ProjectInstaller() as installer =
+    inherit Installer()
+
+    let processInstaller = new ServiceProcessInstaller();
+    let serviceInstaller = new ServiceInstaller();
+
+    // TODO initialize your service
+    let initInstaller = 
+
+        // Define the process settings
+        processInstaller.Account <- ServiceAccount.LocalSystem
+        processInstaller.Password <- null
+        processInstaller.Username <- null
+        
+        // Define the service settings
+        serviceInstaller.Description <- "sync.today.service SyncTodayService"
+        serviceInstaller.ServiceName <- "sync.today.service.SyncTodayService"
+        serviceInstaller.StartType <- ServiceStartMode.Manual;
+        
+        // Define the installers
+        let installers = [| processInstaller :> Installer; serviceInstaller :> Installer|]
+        installer.Installers.AddRange(installers) 
+
+    do
+        initInstaller
---
+++ sync.today.service/Service.fs
@@ -0,0 +1,38 @@
+﻿namespace sync.today.service
+
+open System;
+open System.Diagnostics;
+open System.Linq;
+open System.ServiceProcess;
+open System.Text;
+
+type public SyncTodayService() as service =
+    inherit ServiceBase()
+   
+    // TODO define your service variables
+    let eventLog = new EventLog();
+    
+    // TODO initialize your service
+    let initService = 
+        service.ServiceName <- "sync.today.service" 
+
+        // Define the Event Log
+        let eventSource = "sync.today.service"
+        if not (EventLog.SourceExists(eventSource)) then
+            EventLog.CreateEventSource(eventSource, "Application");
+
+        eventLog.Source <- eventSource;
+        eventLog.Log <- "Application";
+
+    do
+        initService
+
+    // TODO define your service operations
+    override service.OnStart(args:string[]) =
+        base.OnStart(args)
+        eventLog.WriteEntry("Service Started")
+
+    override service.OnStop() =
+        base.OnStop()
+        eventLog.WriteEntry("Service Ended")
+
