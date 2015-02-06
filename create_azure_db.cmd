23120d1ba684f43799e9effb15f916dbfed1d0ee
David Podhola <david.podhola@naseukoly.cz>
2015-02-06 00:22:24 +0000

    create azure db cmd added
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C50

---
+++ create_azure_db.cmd
@@ -0,0 +1,1 @@
+packages\sync.today.createdb.1.0.5515.2227\lib\net45\sync.today.createdb.exe "Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False"
