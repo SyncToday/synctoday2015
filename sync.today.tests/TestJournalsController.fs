36cc78c6fcb97e45b2b56807fa4b1a07bd013d35
David Podhola <david.podhola@naseukoly.cz>
2015-02-13 14:42:34 +0000

    journal testing started
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C71

---
+++ sync.today.tests/TestJournalsController.fs
@@ -0,0 +1,25 @@
+﻿module TestJournalsController
+
+open NUnit.Framework
+open FsUnit
+open JournalsController
+
+
+[<TestFixture>] 
+type ``logging a info`` ()=
+    //[<BeforeTest>] member x.
+
+    let logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+    do logger.Info("Test")
+
+    let JournalsController = 
+        new JournalsController()
+
+    [<Test>] member x.
+     ``when I ask for journals it should not be Null.`` ()=
+            JournalsController.Get |> should not' (be Null)
+
+    [<Test>] member x.
+     ``when I ask for journals it should have more then zero members.`` ()=
+            JournalsController.Get().IsEmpty |> should not' (be True)
