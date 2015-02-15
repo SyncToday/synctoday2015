013ff7b7d4a861fbbe0424745cb8d22bee47da8d
David Podhola <david.podhola@naseukoly.cz>
2015-02-15 17:57:13 +0000

    all tests green!
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C80

---
+++ sync.today.tests/Setup.fs
@@ -0,0 +1,10 @@
+﻿module Setup
+
+open NUnit.Framework
+
+[<SetUpFixture>] 
+type Setup() = 
+    
+    [<SetUp>] 
+    member x.RunBeforeAnyTests() =
+        log4net.Config.XmlConfigurator.Configure() |> ignore
