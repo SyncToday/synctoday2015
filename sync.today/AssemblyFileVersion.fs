f94021d262192eb0984dcd92de36748bdbfd0464
David Podhola <david.podhola@naseukoly.cz>
2015-02-15 23:08:11 +0100

    System.Data.Entity removed in travis fsproj  too

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.15.2232")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.15.2306")>]
       ()
