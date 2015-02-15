e190055c92fb6315f5ec12259a3b253521048090
David Podhola <david.podhola@naseukoly.cz>
2015-02-15 20:34:06 +0000

    Making MainDataConnection Thread-Safe see http://stackoverflow.com/questions/27751823/f-type-provider-in-dal
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C84

--- sync.today/MainDataConnection.fs
+++ sync.today/MainDataConnection.fs
@@ -20,37 +20,37 @@ open Microsoft.FSharp.Data.TypeProviders
 open sync.today.Models
 
 type private SqlConnection = SqlDataConnection<ConnectionStringName="sync-today-mssql">
-let private db = SqlConnection.GetDataContext()
-let private cnn = db.DataContext
+let private db() = SqlConnection.GetDataContext()
+let private cnn() = db().DataContext
 
 let internal journals() = 
     query {
-        for r in db.Journals do
+        for r in db().Journals do
         select { Id = r.Id; Date = r.Date; Thread = r.Thread; Level = r.Level; Logger = r.Logger; Logger_method = r.Logger_method; Message = r.Message; Exception = r.Exception; Stacktrace = r.Stacktrace }
     } |> Seq.toList
 
 let internal workflows() = 
     query {
-        for r in db.Workflows do
+        for r in db().Workflows do
         select { Id = r.Id; CreatedOn = r.CreatedOn; Name = r.Name; XamlCode = r.XamlCode }
     } |> Seq.toList
 
 let internal workflowById( id : int ) = 
     query {
-        for r in db.Workflows do
+        for r in db().Workflows do
         where ( r.Id = id )
         select { Id = r.Id; CreatedOn = r.CreatedOn; Name = r.Name; XamlCode = r.XamlCode }
     } |> Seq.tryHead
 
 let internal processes() = 
     query {
-        for r in db.Processes do
+        for r in db().Processes do
         select { Id = r.Id; StartedOn = r.StartedOn; FinishedOn = r.FinishedOn; Name = r.Name; XamlCode = r.XamlCode; Exception = r.Exception; Workflow = workflowById(r.WorkflowId).Value; IsAlive = false }
     } |> Seq.toList
 
 let internal processById( id : int ) = 
     query {
-        for r in db.Processes do
+        for r in db().Processes do
         where ( r.Id = id )
         select { Id = r.Id; StartedOn = r.StartedOn; FinishedOn = r.FinishedOn; Name = r.Name; XamlCode = r.XamlCode; Exception = r.Exception; Workflow = workflowById(r.WorkflowId).Value; IsAlive = false }
     } |> Seq.tryHead
