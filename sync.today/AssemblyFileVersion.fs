27f6cf4e545877f4f540f258a1498202978c59e3
David Podhola <david.podhola@naseukoly.cz>
2015-02-16 20:45:33 +0000

    afv
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C110

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.16.2132")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.16.2135")>]
       ()
