488a1c9be4739a21212eddd623a86be7c3dc4082
David Podhola <david.podhola@naseukoly.cz>
2015-02-16 23:10:50 +0100

    createdb updated to create Appointments

--- sync.today/Repositories/AppointmentRepository.fs
+++ sync.today/Repositories/AppointmentRepository.fs
@@ -1,6 +1,10 @@
 ﻿module AppointmentRepository
 
 open MainDataConnection
+open sync.today.Models
 
 let Appointments() =
     appointments()
+
+let InsertAppointment( appointment : AppointmentDTO ) =
+    insertAppointment( appointment )
