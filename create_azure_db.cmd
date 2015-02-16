448de3d3947771aa9f0ce1009f230e721447fae9
David Podhola <david.podhola@naseukoly.cz>
2015-02-16 23:29:34 +0100

    running nuget restore in azure.cmd

--- create_azure_db.cmd
+++ create_azure_db.cmd
@@ -1,2 +1,3 @@
+.nuget\NuGet restore sync.today.sln
 packages\sync.today.createdb.1.0.5525.41548\lib\net45\sync.today.createdb.exe "Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False"
 tools\fnr\fnr.exe --cl --find "Server=(localdb)\ProjectsV12; Database=SyncToday2015.new; Trusted_Connection=True;" --replace "Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False" --dir "C:\projects\synctoday2015" --fileMask "*.config" --includeSubDirectories
