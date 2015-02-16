0b19893bcd850ae2077ecf5b8ab51433844e02a0
David Podhola <david.podhola@naseukoly.cz>
2015-02-16 15:07:50 +0000

    private folder, new file ver
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C94

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.15.2306")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.16.1129")>]
       ()
