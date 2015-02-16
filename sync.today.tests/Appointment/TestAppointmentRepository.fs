e56a57db63fab872618f5172b5fd21af7f45135a
David Podhola <david.podhola@naseukoly.cz>
2015-02-16 22:29:34 +0000

    running nuget restore in azure.cmd
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C120

---
+++ sync.today.tests/Appointment/TestAppointmentRepository.fs
@@ -0,0 +1,26 @@
+﻿module TestAppointmentRepository
+
+open NUnit.Framework
+open FsUnit
+open System.Threading.Tasks
+
+[<TestFixture>] 
+type ``logging a info in repository`` ()=
+    //[<BeforeTest>] member x.
+
+    let logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+    [<TestFixtureSetUp>] member x.``Log Test At the beginning`` ()=         
+            logger.Info("Test")
+
+    [<Test>] member x.``when I ask for journals it should not be Null.`` ()=
+            JournalRepository.Journals() |> should not' (be Null)
+
+    [<Test>] member x.``when I ask for journals it should have more then zero members.`` ()=
+             JournalRepository.Journals().IsEmpty |> should not' (be True)
+
+    [<Test>] member x.``when another Thread is logging, the journal should contain the logged data as well.`` () =
+            Parallel.ForEach([1;2], fun x-> 
+                                        logger.Info(x+1) 
+                                        JournalRepository.Journals().IsEmpty |> should not' (be True)                                        
+                                        ) |> ignore            
--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.16.2248")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.16.2310")>]
       ()
