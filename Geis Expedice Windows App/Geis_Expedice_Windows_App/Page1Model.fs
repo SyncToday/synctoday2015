36e23817125a9d436902d5715301709a25e74e94
davidpodhola <david.podhola@naseukoly.cz>
2015-02-18 01:42:11 +0100

    Geis_Expedice_Windows_App working

---
+++ Geis Expedice Windows App/Geis_Expedice_Windows_App/Page1Model.fs
@@ -0,0 +1,31 @@
+﻿namespace Views
+
+open System
+open System.Windows
+open System.Windows.Input
+
+open System.IO
+open System.Text
+
+open FsXaml
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
+
+type Page1ViewModel() as self = 
+    inherit ViewModelBase()
+
+    let okCommand = 
+        self.Factory.CommandSync(
+           (fun param -> ( 
+                            ( 
+                                let result = [| GeisExpedice.output.ToString() |]
+                                let now = DateTime.Now
+                                let fileName = "GEIS-" + now.Year.ToString() + "-" + now.Month.ToString() + "-" + now.Day.ToString() + "-" +  now.Hour.ToString() + "-" + now.Minute.ToString() + "-" + now.Second.ToString() + ".xml"
+                                File.WriteAllLines( fileName, result, Encoding.UTF8 ) 
+
+                            ) |> ignore 
+                          ) )
+        )
+    
+
+    member x.OkCommand = okCommand
