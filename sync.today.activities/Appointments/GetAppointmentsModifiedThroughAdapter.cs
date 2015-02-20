fd2257f102c8871583d4f3124480b6ee3f57b5d9
David Podhola <david.podhola@naseukoly.cz>
2015-02-20 06:15:16 +0000

    workflow work in progress
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C164

---
+++ sync.today.activities/Appointments/GetAppointmentsModifiedThroughAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sync.today.activities.Appointments
+{
+    public sealed class GetAppointmentsModifiedThroughAdapter : CodeActivity
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+    (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public OutArgument<Models.AppointmentDTO[]> result { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            log.Debug("Entered");
+            try
+            {
+                var journalItems = AppointmentRepository.ModifiedThroughAdapter();
+                List<Models.AppointmentDTO> resultItems = new List<Models.AppointmentDTO>(journalItems);
+                result.Set(context, resultItems.ToArray());
+            }
+            catch (Exception ex)
+            {
+                log.Fatal("failed", ex);
+                throw;
+            }
+        }
+
+    }
+}
---
+++ sync.today.activities/Appointments/GetLatestModificationAsWinner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sync.today.activities.Appointments
+{
+    public sealed class GetLatestModificationAsWinner : CodeActivity
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+    (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public InArgument<Models.AdapterAppointmentDTO[]> adapterAppointments { get; set; }
+        public OutArgument<Models.AdapterAppointmentDTO> winner { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            log.Debug(string.Format("Entered for '{0}'", adapterAppointments));
+            try
+            {
+                var myAdapterAppointments = adapterAppointments.Get(context);
+                Models.AdapterAppointmentDTO possibleWinner = null;
+                DateTime lastModified = DateTime.MinValue;
+                foreach (var adapterAppointment in myAdapterAppointments)
+                {
+                    if (adapterAppointment.LastModified > lastModified)
+                    {
+                        lastModified = adapterAppointment.LastModified;
+                        possibleWinner = adapterAppointment;
+                    }
+                }
+                winner.Set(context, possibleWinner);
+            }
+            catch (Exception ex)
+            {
+                log.Fatal("failed", ex);
+                throw;
+            }
+        }
+
+    }}
