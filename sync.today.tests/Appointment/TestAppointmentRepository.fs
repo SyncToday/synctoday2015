e74195473b0b0ef5133a8d23d992c6f6f829cf8c
David Podhola <david.podhola@naseukoly.cz>
2015-02-20 18:40:33 +0000

    a little bit more steps in the test + log4net not internal.debug
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C185

--- sync.today.tests/Appointment/TestAppointmentRepository.fs
+++ sync.today.tests/Appointment/TestAppointmentRepository.fs
@@ -56,3 +56,30 @@ type ``appointment persistence`` ()=
             let appointmentAdapter : AdapterAppointmentDTO = { Id = -1; InternalId = internalId; LastModified = DateTime.Now; Category="";Location="";Content="";Title=""; DateFrom=DateTime.Now; DateTo=DateTime.Now; Reminder=Nullable<DateTime>(); Notification=false; IsPrivate=false; Priority=byte 0; AppointmentId = appId; AdapterId = adapterId; ServiceAccountId = serviceAccountId; Tag = 0}
             AdapterAppointmentRepository.InsertOrUpdate(appointmentAdapter)
 
+
+    [<Test>] member x.``when I create appointment and search among adapter appointments, they should be connected`` ()=
+            let serviceId1 = MainDataConnection.insertService( { Id = 0; Key = "Key1"; Name = "Name1" } )
+            let serviceId2 = MainDataConnection.insertService( { Id = 0; Key = "Key2"; Name = "Name2" } )
+            let adapterId1 = MainDataConnection.insertAdapter( { Id = 0; ServiceId = serviceId1; Name = "Name1" } )
+            let adapterId2 = MainDataConnection.insertAdapter( { Id = 0; ServiceId = serviceId2; Name = "Name2" } )
+            let accountId1 = MainDataConnection.insertAccount( { Id = 0; Name = "Name1" } )
+            let accountId2 = MainDataConnection.insertAccount( { Id = 0; Name = "Name2" } )
+            let serviceAccountId1 = MainDataConnection.insertServiceAccount({Id = 0; LoginJSON = ""; ServiceId = serviceId1; AccountId = accountId1; LastDownload = DateTime.Now })
+            let serviceAccountId2 = MainDataConnection.insertServiceAccount({Id = 0; LoginJSON = ""; ServiceId = serviceId2; AccountId = accountId2; LastDownload = DateTime.Now })
+
+            let internalId = Guid.NewGuid()
+            let appointment : AppointmentDTO = { Id = -1; InternalId = internalId; LastModified = DateTime.Now; Category="";Location="";Content="";Title=""; DateFrom=DateTime.Now; DateTo=DateTime.Now; Reminder=Nullable<DateTime>(); Notification=false; IsPrivate=false; Priority=byte 0}
+            let appId = AppointmentRepository.InsertAppointment( appointment )
+            AppointmentRepository.Appointments().IsEmpty |> should not' (be True)
+            let appointmentAdapter1 : AdapterAppointmentDTO = { Id = -1; InternalId = internalId; LastModified = DateTime.Now; Category="";Location="";Content="";Title=""; 
+                                                                DateFrom=DateTime.Now; DateTo=DateTime.Now; Reminder=Nullable<DateTime>(); Notification=false; IsPrivate=false; 
+                                                                Priority=byte 0; AppointmentId = appId; AdapterId = adapterId1; ServiceAccountId = serviceAccountId1; Tag = 0}
+            AdapterAppointmentRepository.InsertOrUpdate(appointmentAdapter1)
+            let appointmentAdapter2 : AdapterAppointmentDTO = { Id = -1; InternalId = internalId; LastModified = DateTime.Now; Category="";Location="";Content="";Title=""; 
+                                                                DateFrom=DateTime.Now; DateTo=DateTime.Now; Reminder=Nullable<DateTime>(); Notification=false; IsPrivate=false; 
+                                                                Priority=byte 0; AppointmentId = appId; AdapterId = adapterId2; ServiceAccountId = serviceAccountId2; Tag = 0}
+            AdapterAppointmentRepository.InsertOrUpdate(appointmentAdapter2)
+            let adapterAppointments = AdapterAppointmentRepository.AdapterAppointments(appId) 
+            adapterAppointments  |> should not' (be Null)
+            adapterAppointments.Length |> should equal 2
+
