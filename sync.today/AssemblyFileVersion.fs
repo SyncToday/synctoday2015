71be3c82b8e40dbabf8734356cfa9412530841f8
David Podhola <david.podhola@naseukoly.cz>
2015-02-20 15:59:12 +0100

    HintPath fixed in activities to find log4net

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.20.1448")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.20.1457")>]
       ()
