2443337c885983d5502e78b3ab480f57c3bb1fab
David Podhola <david.podhola@naseukoly.cz>
2015-02-22 10:55:04 +0000

    Update AdapterApp
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C197

--- sync.today/Repositories/AdapterAppointmentRepository.fs
+++ sync.today/Repositories/AdapterAppointmentRepository.fs
@@ -26,3 +26,12 @@ let areStandardAttrsVisiblyDifferent( a1 : AdapterAppointmentDTO, a2 : AdapterAp
     ( a1n.Category = a2n.Category ) && ( a1n.Location = a1n.Location ) && ( a1n.Content = a1n.Content ) && ( a1n.Title = a1n.Title )
     && ( a1n.DateFrom = a2n.DateFrom ) && ( a1n.DateTo = a2n.DateTo ) && ( a1n.Reminder = a2n.Reminder ) && ( a1n.Notification = a2n.Notification )
     && ( a1n.IsPrivate = a2n.IsPrivate ) && ( a1n.Priority = a2n.Priority )
+
+let Update( id : Guid,  r : AdapterAppointmentDTO ) = 
+    let db = adapterAppointmentDTOByInternalId( id ).Value
+    let updatedAdapterAppointment = 
+        { Id = db.Id; InternalId = db.InternalId; LastModified = r.LastModified; Category = r.Category; Location = r.Location; Content = r.Content; Title = r.Title; 
+        DateFrom = fixDateSecs(r.DateFrom); DateTo = fixDateSecs(r.DateTo); Reminder = ( if r.Reminder.HasValue then Nullable(fixDateSecs(r.Reminder.Value)) else Nullable() ); 
+        Notification = r.Notification; IsPrivate = r.IsPrivate; Priority = r.Priority; 
+        AppointmentId = db.AppointmentId; AdapterId = db.AdapterId; ServiceAccountId = db.ServiceAccountId; Tag = r.Tag }
+    insertOrUpdate(updatedAdapterAppointment, false)
