d3a131b247fa842f5eb6df249a8c9489bf0073e3
David Podhola <david.podhola@naseukoly.cz>
2015-02-22 11:55:04 +0100

    Update AdapterApp

---
+++ sync.today.activities/Appointments/Adapter/AreStandardAdapterAppointmentsVisiblyDifferent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sync.today.activities.Appointments.Adapter
+{
+    public sealed class AreStandardAdapterAppointmentsVisiblyDifferent : CodeActivity
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+    (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public InArgument<Models.AdapterAppointmentDTO> adapterAppointment1 { get; set; }
+        public InArgument<Models.AdapterAppointmentDTO> adapterAppointment2 { get; set; }
+        public OutArgument<bool> result { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            log.Debug(string.Format("Entered for '{0}' and '{1}'", adapterAppointment1, adapterAppointment2));
+            try
+            {
+                var myAdapterAppointment1 = adapterAppointment1.Get(context);
+                var myAdapterAppointment2 = adapterAppointment2.Get(context);
+                log.Debug(string.Format("would call for '{0}' and '{1}'", myAdapterAppointment1, myAdapterAppointment2));
+                var myResult = AdapterAppointmentRepository.areStandardAttrsVisiblyDifferent(myAdapterAppointment1, myAdapterAppointment2);
+                result.Set(context, myResult);
+            }
+            catch (Exception ex)
+            {
+                log.Fatal("failed", ex);
+                throw;
+            }
+        }
+
+    }}
--- sync.today/Repositories/AdapterAppointmentRepository.fs
+++ sync.today/Repositories/AdapterAppointmentRepository.fs
@@ -1,5 +1,6 @@
 ﻿module AdapterAppointmentRepository
 
+open Common
 open System
 open AdapterAppointmentsSQL
 open sync.today.Models
@@ -12,3 +13,25 @@ let InsertOrUpdate( adapterAppointment : AdapterAppointmentDTO ) =
 
 let Get( id : Guid ) =
     adapterAppointmentDTOByInternalId( id )
+
+let normalize( r : AdapterAppointmentDTO ) : AdapterAppointmentDTO =
+    { Id = r.Id; InternalId = r.InternalId; LastModified = fixDateSecs(r.LastModified); Category = r.Category; Location = r.Location; Content = r.Content; Title = r.Title; 
+    DateFrom = fixDateSecs(r.DateFrom); DateTo = fixDateSecs(r.DateTo); Reminder = ( if r.Reminder.HasValue then Nullable(fixDateSecs(r.Reminder.Value)) else Nullable() ); 
+    Notification = r.Notification; IsPrivate = r.IsPrivate; Priority = r.Priority; 
+    AppointmentId = r.AppointmentId; AdapterId = r.AdapterId; ServiceAccountId = r.ServiceAccountId; Tag = r.Tag }
+
+let areStandardAttrsVisiblyDifferent( a1 : AdapterAppointmentDTO, a2 : AdapterAppointmentDTO ) : bool =
+    let a1n = normalize( a1 )
+    let a2n = normalize( a2 )
+    ( a1n.Category = a2n.Category ) && ( a1n.Location = a1n.Location ) && ( a1n.Content = a1n.Content ) && ( a1n.Title = a1n.Title )
+    && ( a1n.DateFrom = a2n.DateFrom ) && ( a1n.DateTo = a2n.DateTo ) && ( a1n.Reminder = a2n.Reminder ) && ( a1n.Notification = a2n.Notification )
+    && ( a1n.IsPrivate = a2n.IsPrivate ) && ( a1n.Priority = a2n.Priority )
+
+let Update( id : Guid,  r : AdapterAppointmentDTO ) = 
+    let db = adapterAppointmentDTOByInternalId( id ).Value
+    let updatedAdapterAppointment = 
+        { Id = db.Id; InternalId = db.InternalId; LastModified = r.LastModified; Category = r.Category; Location = r.Location; Content = r.Content; Title = r.Title; 
+        DateFrom = fixDateSecs(r.DateFrom); DateTo = fixDateSecs(r.DateTo); Reminder = ( if r.Reminder.HasValue then Nullable(fixDateSecs(r.Reminder.Value)) else Nullable() ); 
+        Notification = r.Notification; IsPrivate = r.IsPrivate; Priority = r.Priority; 
+        AppointmentId = db.AppointmentId; AdapterId = db.AdapterId; ServiceAccountId = db.ServiceAccountId; Tag = r.Tag }
+    insertOrUpdate(updatedAdapterAppointment, false)
