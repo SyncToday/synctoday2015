03c62600883dd01ea18189f2a0fe14c4a5ac52ce
David Podhola <david.podhola@naseukoly.cz>
2015-02-22 12:35:09 +0100

    createdb updated

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.22.0104")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.22.1224")>]
       ()
