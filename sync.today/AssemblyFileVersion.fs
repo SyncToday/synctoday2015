24a128524350528eb1004306c68b6cdc132f731e
David Podhola <david.podhola@naseukoly.cz>
2015-02-25 00:19:11 +0100

    unfinished GetConsumers finished

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.24.2210")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.25.0006")>]
       ()
