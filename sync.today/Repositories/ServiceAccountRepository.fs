ce748ba7d780529d1f20a0be1bb1e2cef0f7b76b
David Podhola <david.podhola@naseukoly.cz>
2015-02-25 01:13:29 +0100

    Exchange.Download implemented

--- sync.today/Repositories/ServiceAccountRepository.fs
+++ sync.today/Repositories/ServiceAccountRepository.fs
@@ -7,6 +7,11 @@ open sync.today.Models
 let ServiceAccounts()  = 
     serviceAccounts()
 
-let Download( serviceAccount : ServiceAccountDTO ) =
-    0 |> ignore
+let Download( serviceAccount : ServiceAccountDTO, doDownload : ServiceAccountDTO -> unit ) =
+    let serviceAccountPre = { serviceAccount with LastDownloadAttempt = Nullable(DateTime.Now) }
+    insertOrUpdate( serviceAccountPre )
+    doDownload( serviceAccountPre )
+    let serviceAccountPost = { serviceAccount with LastSuccessfulDownload = Nullable(DateTime.Now) }
+    insertOrUpdate( serviceAccountPost )
+
 
