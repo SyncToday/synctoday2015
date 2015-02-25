24670adc320789eba5875719770b1a7adea19c0c
David Podhola <david.podhola@naseukoly.cz>
2015-02-25 12:32:04 +0000

    at least nuget working now
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C241

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.25.0006")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.25.1321")>]
       ()
