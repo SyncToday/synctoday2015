7d63644c160f47913154a6fc50ffe9e7c26ddd4e
David Podhola <david.podhola@naseukoly.cz>
2015-02-26 13:15:17 +0000

    EnsureAdapter
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C263

--- sync.today.activities/Adapters/GetAdapters.cs
+++ sync.today.activities/Adapters/GetAdapters.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace sync.today.activities
+namespace sync.today.activities.Adapters
 {
     public sealed class GetAdapters : CodeActivity
     {
