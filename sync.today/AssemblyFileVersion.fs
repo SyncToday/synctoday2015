bc8ac354e98e313bb9e8405615c64018bddbb3dc
davidpodhola <david.podhola@naseukoly.cz>
2015-02-27 12:20:46 +0100

    afv fixed

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,7 +1,3 @@
 module sync.today.AssemblyFileVersion
-<<<<<<< HEAD
       [<assembly:System.Reflection.AssemblyFileVersion("15.02.27.1151")>]
-=======
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.27.1136")>]
->>>>>>> tfs/default
       ()
