68358e8fa606187287c08852830b4d975688a8cc
davidpodhola <david.podhola@naseukoly.cz>
2015-02-27 11:53:21 +0100

    merge marks commit fixed

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,7 +1,3 @@
 module sync.today.AssemblyFileVersion
-<<<<<<< HEAD
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.27.0049")>]
-=======
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.27.1135")>]
->>>>>>> tfs/default
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.27.1151")>]
       ()
