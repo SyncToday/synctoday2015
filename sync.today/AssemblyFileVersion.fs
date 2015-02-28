7445042d20ae47818a4fb8c38507633f611999bb
David Podhola <david.podhola@naseukoly.cz>
2015-03-01 00:11:57 +0100

    Exchange.Upload

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.28.0826")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.02.28.0902")>]
       ()
