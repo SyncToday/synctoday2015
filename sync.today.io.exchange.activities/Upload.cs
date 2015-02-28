8d82e85d648507de5fdc3ef442d75334bcf85c2c
David Podhola <david.podhola@naseukoly.cz>
2015-02-28 23:11:57 +0000

    Exchange.Upload
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C299

---
+++ sync.today.io.exchange.activities/Upload.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sync.today.io.exchange.activities
+{
+    public sealed class Upload : CodeActivity
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+    (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public InArgument<Models.ServiceAccountDTO> ServiceAccount { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            try
+            {
+                log.Debug(string.Format("Entered for '{0}'", ServiceAccount));
+                var myServiceAccount = ServiceAccount.Get(context);
+                log.Debug(string.Format("Got for '{0}'", myServiceAccount));
+                ExchangeRepository.Upload(myServiceAccount);
+            }
+            catch (Exception ex)
+            {
+                log.Fatal("failed", ex);
+                throw;
+            }
+        }
+
+    }
+}
