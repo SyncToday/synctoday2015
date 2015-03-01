223c74b64606ce98d980a8edbcb4740d5238aee3
davidpodhola <david.podhola@naseukoly.cz>
2015-03-01 09:30:21 +0100

    NUnit Test Adapter added as a package so all developers can run our tests

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.01.0102")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.01.0914")>]
       ()
