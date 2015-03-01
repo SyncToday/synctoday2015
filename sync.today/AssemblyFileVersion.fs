2859e7568a797d38de91ffa44c2ecf8510f87af0
David Podhola <david.podhola@naseukoly.cz>
2015-03-01 01:03:22 +0000

    more ExchangeRepository tests to make sure InternalId is not changed
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C302

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.02.28.0902")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.01.0102")>]
       ()
