ea5d196d8d89305cee00b5bcd926f4b0f41cdc25
David Podhola <david.podhola@naseukoly.cz>
2015-03-02 11:26:34 +0000

    createdb package updated
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C322

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.02.0924")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.02.1223")>]
       ()
