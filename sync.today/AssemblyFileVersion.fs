0d9c46bab82b8e239d35bb8fdd044838026f54c7
David Podhola <david.podhola@naseukoly.cz>
2015-03-03 00:21:00 +0100

    afv

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.02.1407")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.02.2334")>]
       ()
