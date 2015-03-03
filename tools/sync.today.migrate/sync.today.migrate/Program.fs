3aa4c51705219d51aa9b37b78045c86419f2c62b
David Podhola <david.podhola@naseukoly.cz>
2015-03-03 13:01:21 +0000

    buildable
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C338

--- tools/sync.today.migrate/sync.today.migrate/Program.fs
+++ tools/sync.today.migrate/sync.today.migrate/Program.fs
@@ -4,6 +4,6 @@
 let main argv = 
     
     let oldUsers = OldDataConnection.users()
-//    let newUsers = 
+    let newUsers = ConsumerRepository.Consumers()
 
     0 // return an integer exit code
