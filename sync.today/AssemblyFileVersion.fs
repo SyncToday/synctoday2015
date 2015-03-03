931c3c4a16cdcdd4a497e29d8c1130fd57e2223b
David Podhola <david.podhola@naseukoly.cz>
2015-03-03 21:29:35 +0000

    LogFatal activity
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C352

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.03.2119")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.03.1953")>]
       ()
