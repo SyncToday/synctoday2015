2e839451f326b8ffb90782cf5062ea7a79e780c1
David Podhola <david.podhola@naseukoly.cz>
2015-03-03 07:21:21 +0000

    sync.today.migrate started
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C333

---
+++ tools/sync.today.migrate/sync.today.migrate/OldDataConnection.fs
@@ -0,0 +1,8 @@
+﻿module OldDataConnection
+
+open Microsoft.FSharp.Data.TypeProviders
+
+type internal SqlConnection = SqlDataConnection<ConnectionStringName="sync-today-mssql-old">
+let internal db() = SqlConnection.GetDataContext()
+let internal cnn() = db().DataContext
+
