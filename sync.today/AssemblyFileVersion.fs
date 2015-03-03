1ab1ef2f8152c5f5f8038f2c813e02f6b6a50b42
David Podhola <david.podhola@naseukoly.cz>
2015-03-03 17:54:18 +0000

    consumerByName
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C341

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.03.1108")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.03.1341")>]
       ()
