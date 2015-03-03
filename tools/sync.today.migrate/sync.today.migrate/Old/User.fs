e87e04e69d9f78c5fe2d98dc2179751be46e29a8
David Podhola <david.podhola@naseukoly.cz>
2015-03-03 07:36:26 +0000

    OldUser
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C334

---
+++ tools/sync.today.migrate/sync.today.migrate/Old/User.fs
@@ -0,0 +1,15 @@
+﻿namespace sync.today.Old
+
+open System
+
+[<CLIMutable>]
+type OldUserDTO =
+    {   Id : int
+        InternalId : Guid
+        UserId : int
+        FirstName : string
+        LastName : string
+        Email : string
+    }
+    override m.ToString() = sprintf "[%A] %A %A (%A-%A)" m.Id m.FirstName m.LastName  m.InternalId m.Email
+        
