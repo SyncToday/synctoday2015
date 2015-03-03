d314d0d7d2a889493d5cf2af825a6657b20fe061
David Podhola <david.podhola@naseukoly.cz>
2015-03-03 22:39:39 +0000

    sync.today.io.google started
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C354

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.03.1953")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.03.2119")>]
       ()
