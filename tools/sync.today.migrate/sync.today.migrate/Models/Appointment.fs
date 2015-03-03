5850f26303f98777773505bf092874cf783b32c3
David Podhola <david.podhola@naseukoly.cz>
2015-03-03 08:21:21 +0100

    sync.today.migrate started

---
+++ tools/sync.today.migrate/sync.today.migrate/Models/Appointment.fs
@@ -0,0 +1,22 @@
+﻿namespace sync.today.Models
+
+open System
+
+[<CLIMutable>]
+type AppointmentDTO =
+    {   Id : int
+        InternalId : Guid
+        LastModified : DateTime
+        Category : string
+        Location : string
+        Content : string
+        Title : string
+        DateFrom: DateTime
+        DateTo: DateTime
+        Reminder: Nullable<DateTime>
+        Notification : bool
+        IsPrivate : bool
+        Priority : byte
+        ConsumerId : int
+    }
+
---
+++ tools/sync.today.migrate/sync.today.migrate/Models/ServiceAccount.fs
@@ -0,0 +1,16 @@
+﻿namespace sync.today.Models
+
+open System
+
+[<CLIMutable>]
+type ServiceAccountDTO =
+    {   Id : int
+        LoginJSON : string
+        ServiceId : int
+        AccountId : int
+        LastSuccessfulDownload : Nullable<DateTime>
+        LastDownloadAttempt: Nullable<DateTime>
+        LastSuccessfulUpload : Nullable<DateTime>
+        LastUploadAttempt: Nullable<DateTime>
+    }
+    override m.ToString() = sprintf "[%A] [%A] [%A] (%A/%A)" m.Id m.ServiceId m.AccountId m.LastSuccessfulDownload m.LastSuccessfulUpload
