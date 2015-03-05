8172399ccb250f78a412092e8315714954c62364
David Podhola <david.podhola@naseukoly.cz>
2015-03-05 17:29:43 +0000

    Service parameter added to GetServiceAccount
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C371

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.04.1007")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.05.1527")>]
       ()
