572621111aea9e00875d72056439cc791d9de552
davidpodhola <david.podhola@naseukoly.cz>
2015-03-05 09:39:39 +0100

    logging in Geis_Expedice_Windows_App

--- Geis Expedice Windows App/Geis_Expedice_Windows_App/App.fs
+++ Geis Expedice Windows App/Geis_Expedice_Windows_App/App.fs
@@ -5,7 +5,11 @@ open FsXaml
 
 type App = XAML<"App.xaml">
 
+
 [<STAThread>]
 [<EntryPoint>]
 let main argv =
+    log4net.Config.XmlConfigurator.Configure() |> ignore
+    let logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    logger.Info("Application started")    
     App().Root.Run()
--- Geis Expedice Windows App/Geis_Expedice_Windows_App/Page1Model.fs
+++ Geis Expedice Windows App/Geis_Expedice_Windows_App/Page1Model.fs
@@ -11,17 +11,29 @@ open FsXaml
 open FSharp.ViewModule
 open FSharp.ViewModule.Validation
 
+
 type Page1ViewModel() as self = 
     inherit ViewModelBase()
+
+    let logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
     let okCommand = 
         self.Factory.CommandSync(
            (fun param -> ( 
                             ( 
-                                let result = [| GeisExpedice.output.ToString() |]
-                                let now = DateTime.Now
-                                let fileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\GEIS-" + now.Year.ToString() + "-" + now.Month.ToString() + "-" + now.Day.ToString() + "-" +  now.Hour.ToString() + "-" + now.Minute.ToString() + "-" + now.Second.ToString() + ".xml"
-                                File.WriteAllLines( fileName, result, Encoding.UTF8 ) 
+                                try
+                                    logger.Debug("Export started")
+                                    let result = [| GeisExpedice.output.ToString() |]
+                                    let now = DateTime.Now
+                                    let fileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\GEIS-" + now.Year.ToString() + "-" + now.Month.ToString() + "-" + now.Day.ToString() + "-" +  now.Hour.ToString() + "-" + now.Minute.ToString() + "-" + now.Second.ToString() + ".xml"
+                                    logger.DebugFormat("FileName:'{0}'", fileName)
+                                    File.WriteAllLines( fileName, result, Encoding.UTF8 ) 
+                                    GeisExpedice.markAsExported( fileName )
+                                    logger.Debug("Export done")
+                                with
+                                    | ex -> 
+                                        logger.Fatal("Export failed", ex)
+                                        MessageBox.Show( "Export se nepodařil, více informací najdete v logu" ) |>ignore
 
                             ) |> ignore 
                           ) )
