8709a3ecc936d1b93f12a6599c4101350bdd424e
David Podhola <david.podhola@naseukoly.cz>
2015-03-09 07:39:10 +0100

    areStandardAttrsVisiblyDifferent in repositories not to save native data if they are the same

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.07.0302")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.09.0715")>]
       ()
