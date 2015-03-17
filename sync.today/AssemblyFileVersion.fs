d492e2ac722b276594a36327f47b4b297e408be4
David Podhola <david.podhola@naseukoly.cz>
2015-03-17 16:35:47 +0100

    packages upgraded; 1 master.config file from program files used

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.17.0031")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.17.1631")>]
       ()
