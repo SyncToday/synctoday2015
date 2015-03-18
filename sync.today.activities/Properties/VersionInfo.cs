aa131c40f72403b155dfee58f94075095fe03c16
David Podhola <david.podhola@naseukoly.cz>
2015-03-18 16:10:09 +0000

    ExchangeContacts, FloresFirm, FloresPerson
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C442

--- sync.today.activities/Properties/VersionInfo.cs
+++ sync.today.activities/Properties/VersionInfo.cs
@@ -1,1 +1,1 @@
-[assembly:System.Reflection.AssemblyFileVersion("15.03.18.1624")]
+[assembly:System.Reflection.AssemblyFileVersion("15.03.18.1709")]
---
+++ sync.today/Models/ExchangeContact.fs
@@ -0,0 +1,47 @@
+﻿namespace sync.today.Models
+
+open System
+
+[<CLIMutable>]
+type ExchangeContactDTO =
+    {   Id : int
+        InternalId : Guid
+        ExternalId : string
+        LastModifiedTime : DateTime
+
+        JobTitle : string
+        CompanyName : string
+        EmailAddress1 : string
+        EmailAddress2 : string
+        EmailAddress3 : string
+        GivenName : string
+        MiddleName : string
+        Surname : string
+        Alias : string
+        NickName : string
+        HomePhone : string
+        MobilePhone : string
+        BusinessPhone : string
+        PrimaryPhone : string
+        OtherTelephone : string
+        HomeAddressCity : string
+        HomeAddressCountryOrRegion : string
+        HomeAddressPostalCode : string
+        HomeAddressState : string
+        HomeAddressStreet : string
+        OtherAddressCity : string
+        OtherAddressPostalCode : string
+        OtherAddressCountryOrRegion : string
+        OtherAddressState : string
+        OtherAddressStreet : string
+        BusinessAddressCity : string
+        BusinessAddressCountryOrRegion : string
+        BusinessAddressState : string
+        BusinessAddressStreet : string
+        BusinessAddressPostalCode : string
+        
+        CategoriesJSON : string
+        ServiceAccountId : int
+        Tag : int
+    }
+    override m.ToString() = sprintf "[%A] (%A-%A) %A %A [%A]" m.Id m.InternalId m.ExternalId m.GivenName m.Surname m.ServiceAccountId 
