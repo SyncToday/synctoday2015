010853dc72f03d6df7a13992fef07b985c94b469
David Podhola <david.podhola@naseukoly.cz>
2015-03-19 19:20:37 +0100

    travis sln + fsproj

--- sync.today.core/AssemblyFileVersion.fs
+++ sync.today.core/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.19.0108")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.19.0332")>]
       ()
