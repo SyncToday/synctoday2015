e8d428013627ce05ad4f8375cd513f40ef7e102e
David Podhola <david.podhola@naseukoly.cz>
2015-03-22 09:56:54 +0000

    upgraded to the latest version
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C486

--- sync.today.core/AssemblyFileVersion.fs
+++ sync.today.core/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.22.0846")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.22.1052")>]
       ()
