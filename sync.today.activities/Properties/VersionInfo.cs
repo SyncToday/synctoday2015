cd76cd4100af6bdc1197690bc5195ceecc51702e
David Podhola <david.podhola@naseukoly.cz>
2015-03-23 21:13:34 +0000

    sync.today.orleans.startup working, needs a silo
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C501

--- sync.today.activities/Properties/VersionInfo.cs
+++ sync.today.activities/Properties/VersionInfo.cs
@@ -1,1 +1,1 @@
-[assembly:System.Reflection.AssemblyFileVersion("15.03.23.1727")]
+[assembly:System.Reflection.AssemblyFileVersion("15.03.23.2131")]
--- sync.today.orleans.startup/Starter.fs
+++ sync.today.orleans.startup/Starter.fs
@@ -1,9 +1,11 @@
-﻿module Main
+﻿module Starter
 
 open System
 open System.Net.Http
 open FSharp.Configuration
 open Microsoft.Owin.Hosting
+open Orleans
+open sync.today.orleans.interfaces
 
 type Settings = AppSettings<"app.config">
 
@@ -11,10 +13,17 @@ let start(appName : string) =
     EnsureConfigFile.FromMasterConfigForApp(appName)
     EnsureConfigFile.FromMasterConfigForLibrary()
     EnsureConfigFile.FromMasterConfigForWeb()
+    EnsureConfigFile.FromMasterOrleansClient()
 
-    Orleans.GrainClient.Initialize("SyncTodayClientConfiguration.xml")
+    Orleans.GrainClient.Initialize(AppDomain.CurrentDomain.BaseDirectory + "\\SyncTodayClientConfiguration.xml")
 
     let baseAddress = "http://localhost:" + Settings.ServerPort.ToString()
     let server = WebApp.Start<sync.today.Startup>(baseAddress)
+    
+    let test = GrainFactory.GetGrain<ILogPageBreak>(int64 0)
+    test.Log() |> ignore
+            
     server
 
+let stop( server : IDisposable ) =
+    server.Dispose()
