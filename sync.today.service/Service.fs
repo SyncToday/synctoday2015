be63030ec20db45075ebb88a07f5b0c12504f2b8
David Podhola <david.podhola@naseukoly.cz>
2015-03-23 23:13:00 +0100

    service installation working

--- sync.today.service/Service.fs
+++ sync.today.service/Service.fs
@@ -1,4 +1,4 @@
-﻿namespace sync.today.service
+﻿module Service
 
 open Microsoft.Owin.Hosting
 open System;
@@ -8,6 +8,22 @@ open System.ServiceProcess;
 open System.Text;
 open System.Net.Http
 open FSharp.Configuration
+open System.Configuration.Install
+open System.Reflection
+
+let serviceName = "Sync.Today Main Service"
+let mutable server = null
+
+let IsServiceInstalled() =
+    ServiceController.GetServices().Any(fun s -> s.ServiceName = serviceName )
+
+let UninstallService() =
+    ManagedInstallerClass.InstallHelper([| "/u"; Assembly.GetExecutingAssembly().Location |] );
+
+let InstallService() =
+    if IsServiceInstalled() then
+        UninstallService()
+    ManagedInstallerClass.InstallHelper([| Assembly.GetExecutingAssembly().Location |] );
 
 type Settings = AppSettings<"app.config">
 
@@ -15,14 +31,12 @@ type public Main() as service =
     inherit ServiceBase()
    
     let eventLog = new EventLog();
-
-    let mutable server = null
     
     let initService = 
-        service.ServiceName <- "sync.today.service" 
+        service.ServiceName <- serviceName
 
         // Define the Event Log
-        let eventSource = "sync.today.service"
+        let eventSource = serviceName
         if not (EventLog.SourceExists(eventSource)) then
             EventLog.CreateEventSource(eventSource, "Application");
 
@@ -51,4 +65,3 @@ type public Main() as service =
             eventLog.WriteEntry("Service Ended")
         with
           |  ex -> eventLog.WriteEntry( ( sprintf "Service Stop Failed:\n%A" ex), EventLogEntryType.Error)
-
