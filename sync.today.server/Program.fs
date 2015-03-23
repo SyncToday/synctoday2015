7903788795ad703cca197a92f3324903750fbffb
David Podhola <david.podhola@naseukoly.cz>
2015-03-23 05:23:27 +0000

    sync.today.orleans.startup started
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C493

--- sync.today.server/Program.fs
+++ sync.today.server/Program.fs
@@ -11,7 +11,7 @@ let main argv =
     let baseAddress = "http://localhost:" + Settings.ServerPort.ToString()
     let server = WebApp.Start<sync.today.Startup>(baseAddress)
     //really dumb way to lock console, 
-    //server should be wraped in windows service 
+    //server should be wrapped in windows service 
     Console.WriteLine("Press enter to exit ... ")
     Console.ReadLine() |> ignore
     server.Dispose() 
