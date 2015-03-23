d9dc8706c17597dfa35dc000ad9565578c073c7b
David Podhola <david.podhola@naseukoly.cz>
2015-03-23 06:42:05 +0100

    OrleansStartup start returning the web server instance

---
+++ sync.today.orleans.startup/Main.fs
@@ -0,0 +1,20 @@
+﻿module Main
+
+open System
+open System.Net.Http
+open FSharp.Configuration
+open Microsoft.Owin.Hosting
+
+type Settings = AppSettings<"app.config">
+
+let start(appName : string) =
+    EnsureConfigFile.FromMasterConfigForApp(appName)
+    EnsureConfigFile.FromMasterConfigForLibrary()
+    EnsureConfigFile.FromMasterConfigForWeb()
+
+    Orleans.GrainClient.Initialize("SyncTodayClientConfiguration.xml")
+
+    let baseAddress = "http://localhost:" + Settings.ServerPort.ToString()
+    let server = WebApp.Start<sync.today.Startup>(baseAddress)
+    server
+
