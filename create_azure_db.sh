8090950b1e0c0e2ca01ed90b72c1384c0563070f
David Podhola <david.podhola@naseukoly.cz>
2015-03-23 05:58:56 +0100

    Delete create_azure_db.sh

--- create_azure_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ./packages/sync.today.createdb.1.0.5555.28585/lib/net45/
-mono sync.today.createdb.exe "Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False"
