ec2dec69f4f8f25ac27fbbc17edc8d7632eaff78
David Podhola <david.podhola@naseukoly.cz>
2015-03-24 18:23:09 +0000

    AppVeyor sln updated to include wf.dyn
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C517

--- sync.today.core/AssemblyFileVersion.fs
+++ sync.today.core/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.24.1826")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.24.1919")>]
       ()
