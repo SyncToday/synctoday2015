424c057850377504c35755ff18a94f633bdcba9c
David Podhola <david.podhola@naseukoly.cz>
2015-03-24 21:07:41 +0000

    dynamic workflows from files working (test included)
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C518

--- sync.today.activities/Properties/VersionInfo.cs
+++ sync.today.activities/Properties/VersionInfo.cs
@@ -1,1 +1,1 @@
-[assembly:System.Reflection.AssemblyFileVersion("15.03.24.1921")]
+[assembly:System.Reflection.AssemblyFileVersion("15.03.24.2203")]
--- sync.today.workflow.wf/WorkflowRunner.fs
+++ sync.today.workflow.wf/WorkflowRunner.fs
@@ -4,18 +4,20 @@ open System.Activities.XamlIntegration
 open System.Activities
 open System.IO
 open sync.today.workflow.wf.dyn.Activities
+open System
+open System.Xaml
 
-let runFromFile(filename : string) = 
+let runFromFile(dir : string, filename : string) = 
+    // http://blogs.msdn.com/b/dglick/archive/2010/01/14/referencing-dynamic-activities-reusing-workflow-activities-without-compilation.aspx
+    let schemaContext = new DynamicActivitySchemaContext();
+    schemaContext.RegisterNamespace(new Uri("http://WorkflowConsoleApplication1/"), dir)
     // http://stackoverflow.com/questions/14229129/workflow-foundation-4-5-expression-activity-type-csharpvalue1-requires-compi
     let settings = ActivityXamlServicesSettings( CompileExpressions = true )
-    let xaml = File.ReadAllText(filename)
-    let reader = new StringReader(xaml)
+    let reader = new XamlXmlReader(filename, schemaContext)
     let activity = ActivityXamlServices.Load(reader, settings)
     WorkflowInvoker.Invoke(activity) |> ignore
 
 let runString(xamlWorkflowContent : string) = 
-    // http://blogs.msdn.com/b/dglick/archive/2010/01/14/referencing-dynamic-activities-reusing-workflow-activities-without-compilation.aspx
-    let schemaContext = new DynamicActivitySchemaContext();
     // http://stackoverflow.com/questions/14229129/workflow-foundation-4-5-expression-activity-type-csharpvalue1-requires-compi
     let settings = ActivityXamlServicesSettings( CompileExpressions = true )
     let reader = new StringReader(xamlWorkflowContent)
