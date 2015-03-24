4c5c46e776e072174ca0ab3ab097aa944168e9bf
David Podhola <david.podhola@naseukoly.cz>
2015-03-24 22:07:41 +0100

    dynamic workflows from files working (test included)

--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,3 +1,3 @@
 module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.03.24.1919")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.03.24.1922")>]
       ()
