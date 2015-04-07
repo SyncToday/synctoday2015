6bb4adf8e4d397400579069b047cc1e7113f4a36
David Podhola <david.podhola@naseukoly.cz>
2015-04-07 13:10:59 +0000

    afv
    git-tfs-id: [https://davidpodhola.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C566

--- sync.today.activities/Properties/VersionInfo.cs
+++ sync.today.activities/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 #if RELEASE
-      [assembly:System.Reflection.AssemblyFileVersion("15.04.07.1407")]
+      [assembly:System.Reflection.AssemblyFileVersion("15.04.07.1412")]
       #endif
--- sync.today/AssemblyFileVersion.fs
+++ sync.today/AssemblyFileVersion.fs
@@ -1,5 +1,5 @@
 module sync.today.AssemblyFileVersion
 #if RELEASE
-[<assembly:System.Reflection.AssemblyFileVersion("15.04.07.1325")>]
+[<assembly:System.Reflection.AssemblyFileVersion("15.04.07.1409")>]
       ()
 #endif
