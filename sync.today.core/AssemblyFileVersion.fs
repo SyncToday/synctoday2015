87a695ad04ce073958c286eb39398f2f4e050d66
David Podhola <david.podhola@naseukoly.cz>
2015-04-11 17:24:44 +0200

    travis

--- sync.today.core/AssemblyFileVersion.fs
+++
@@ -1,3 +0,0 @@
-module sync.today.AssemblyFileVersion
-      [<assembly:System.Reflection.AssemblyFileVersion("15.04.10.0021")>]
-      ()
--- sync.today/AssemblyFileVersion.fs
+++
@@ -1,4 +0,0 @@
-module sync.today.AssemblyFileVersion
-#if RELEASE
-[<assembly:System.Reflection.AssemblyFileVersion("15.04.11.1600")>]
-#endif
