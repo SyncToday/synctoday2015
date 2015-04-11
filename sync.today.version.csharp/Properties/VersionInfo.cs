2312707aa5221cb19ec33c0e1dab993a85e9dd37
David Podhola <david.podhola@naseukoly.cz>
2015-04-11 17:33:33 +0200

    afv

--- sync.today.version.csharp/Properties/VersionInfo.cs
+++ sync.today.version.csharp/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 #if RELEASE
-      [assembly:System.Reflection.AssemblyFileVersion("15.04.11.1721")]
+      [assembly:System.Reflection.AssemblyFileVersion("15.04.11.1730")]
       #endif
--- sync.today.version.fsharp/AssemblyFileVersion.fs
+++ sync.today.version.fsharp/AssemblyFileVersion.fs
@@ -1,4 +1,4 @@
 module sync.today.AssemblyFileVersion
 #if RELEASE      
-      [<assembly:System.Reflection.AssemblyFileVersion("15.04.11.1721")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.04.11.1713")>]
 #endif
