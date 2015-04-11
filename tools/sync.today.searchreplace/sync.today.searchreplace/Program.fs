2ee03e6b717c111c3b709edeeaeb99944d31297d
davidpodhola <david.podhola@naseukoly.cz>
2015-04-11 15:12:15 +0200

    fix
    /home/travis/build/SyncToday/synctoday2015/sync.today.core/DB/JournalsSQL.fs(10,21): error FS3033: The type provider 'FSharp.Data.SqlCommandProvider' reported an error: Keyword not supported : 'trustservercertificate'.

--- tools/sync.today.searchreplace/sync.today.searchreplace/Program.fs
+++ tools/sync.today.searchreplace/sync.today.searchreplace/Program.fs
@@ -1,7 +1,7 @@
 ﻿open System.IO
 
 let oldCnn = """Server=(localdb)\ProjectsV12; Database=SyncToday2015.new; Trusted_Connection=True;"""
-let newCnn = """Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False"""
+let newCnn = """Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False"""
 
 [<EntryPoint>]
 let main argv = 
