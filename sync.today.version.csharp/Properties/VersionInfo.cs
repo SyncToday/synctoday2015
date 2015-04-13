2c5e8456105d07b407ff60006f1f896ff06c8c8e
David Podhola <david.podhola@naseukoly.cz>
2015-04-13 13:01:59 +0000

    after rebuild all
    git-tfs-id: [https://synctoday.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C8

--- sync.today.version.csharp/Properties/VersionInfo.cs
+++ sync.today.version.csharp/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 #if RELEASE
-      [assembly:System.Reflection.AssemblyFileVersion("15.04.13.1454")]
+      [assembly:System.Reflection.AssemblyFileVersion("15.04.13.1501")]
       #endif
--- sync.today.version.fsharp/AssemblyFileVersion.fs
+++ sync.today.version.fsharp/AssemblyFileVersion.fs
@@ -1,4 +1,4 @@
 module sync.today.AssemblyFileVersion
 #if RELEASE      
-      [<assembly:System.Reflection.AssemblyFileVersion("15.04.13.1454")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.04.13.1451")>]
 #endif
