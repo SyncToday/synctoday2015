2b832120c91b9c64039260721fd5ba43a45b9e3e
David Podhola <david.podhola@naseukoly.cz>
2015-04-13 20:52:05 +0000

    no nuget restore in _vs
    git-tfs-id: [https://synctoday.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C21

--- create_azure_db_vs.cmd
+++ create_azure_db_vs.cmd
@@ -1,3 +1,2 @@
-Main\2015\.nuget\NuGet restore sync.today.sln
 Main\2015\packages\sync.today.createdb.1.0.5576.36946\lib\net45\sync.today.createdb.exe "Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False"
 Main\2015\tools\fnr\fnr.exe --cl --find "Server=(localdb)\ProjectsV12; Database=SyncToday2015.new; Trusted_Connection=True;" --replace "Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False" --dir "C:\projects\synctoday2015" --fileMask "*.config" --includeSubDirectories
