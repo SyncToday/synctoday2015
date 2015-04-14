b67d047d346fe04b6a44db7ace96386d166a985b
davidpodhola <david.podhola@naseukoly.cz>
2015-04-15 01:39:04 +0200

    EnsureProductSoldToCustomerQuery started

--- sync.today.ent.customers/ProductsSoldToCustomers.fs
+++ sync.today.ent.customers/ProductsSoldToCustomers.fs
@@ -1,6 +1,13 @@
 ﻿module ProductsSoldToCustomers
 
+open System
 open Common
 open FSharp.Data
+open sync.today.Models
 
 type private ProductsSoldToCustomersQuery = SqlCommandProvider<"ProductsSoldToCustomers.sql", ConnectionStringName>
+type private EnsureProductSoldToCustomerQuery = SqlCommandProvider<"EnsureProductSoldToCustomer.sql", ConnectionStringName>
+
+
+let ensureProductSoldToCustomer( product : ProductDTO, customer : CustomerDTO, soldWhen : DateTime, unitPrice : decimal, units : decimal) : ProductSoldToCustomerDTO =
+    ( new EnsureProductSoldToCustomerQuery() ).AsyncExecute(product.Id, customer.Id, soldWhen, unitPrice, units) |> Async.RunSynchronously |> Seq.head |> convert2
