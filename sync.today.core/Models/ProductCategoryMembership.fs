5cf8a297449487f8117c7100975cc2c9a1a1700a
David Podhola <david.podhola@naseukoly.cz>
2015-04-14 08:58:13 +0000

    product and customer models
    git-tfs-id: [https://synctoday.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C38

---
+++ sync.today.core/Models/ProductCategoryMembership.fs
@@ -0,0 +1,14 @@
+﻿namespace sync.today.Models
+
+open System
+
+[<CLIMutable>]
+type ProductCategoryMembershipDTO =
+    {   Id : int
+        InternalId : Guid
+        LastModified : DateTime
+        Category : string
+        ProductId : int
+        ProductCategoryId : string
+    }
+    override m.ToString() = sprintf "[%A] %A" m.Id m.InternalId
