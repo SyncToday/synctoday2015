5cc4bfa71ec7c29044e8e6001cf4f2bc3fa1cbcb
David Podhola <david.podhola@naseukoly.cz>
2015-04-14 11:09:59 +0000

    autogen files
    git-tfs-id: [https://synctoday.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C43

--- sync.today.version.fsharp/AssemblyFileVersion.fs
+++ sync.today.version.fsharp/AssemblyFileVersion.fs
@@ -1,4 +1,4 @@
 module sync.today.AssemblyFileVersion
 #if RELEASE      
-      [<assembly:System.Reflection.AssemblyFileVersion("15.04.14.1115")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.04.14.1307")>]
 #endif
