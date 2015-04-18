d48d96fede637752f7a3b5a98c17d834386d49ef
davidpodhola <david.podhola@naseukoly.cz>
2015-04-18 12:03:15 +0200

    Newtonsoft.Json upgraded to 6

--- sync.today.version.fsharp/AssemblyFileVersion.fs
+++ sync.today.version.fsharp/AssemblyFileVersion.fs
@@ -1,4 +1,4 @@
 module sync.today.AssemblyFileVersion
 #if RELEASE      
-      [<assembly:System.Reflection.AssemblyFileVersion("15.04.16.0553")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.04.18.1142")>]
 #endif
