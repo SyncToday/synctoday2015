b7866fe77167628d20e1b67b748410fcbd7e33b3
davidpodhola <david.podhola@naseukoly.cz>
2015-04-21 21:08:37 +0200

    CalDAV download done with configurable tests

---
+++ sync.today.tests/io.caldav/TestCalDAV.fs
@@ -0,0 +1,37 @@
+﻿module TestCalDAV
+
+open NUnit.Framework
+open FsUnit
+open System.Threading.Tasks
+open sync.today.cipher
+open FSharp.Configuration
+open System
+
+type Settings = AppSettings<"app.config">
+
+[<TestFixture>] 
+type ``working with CalDAV`` ()=
+    //[<BeforeTest>] member x.
+
+    let logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+    [<TestFixtureSetUp>] 
+    member x.``Log Test At the beginning`` ()=         
+        logger.Info("Test")
+
+    [<Test>] 
+    member x.``when I download CalDAV events, should not throw exceptions`` ()=
+        let _userName = Settings.CalDavUserName
+        if String.IsNullOrWhiteSpace(_userName) then
+            Assert.Ignore()
+        let _password = Settings.CalDavPassword
+        let _server = Settings.CalDavServer
+
+        let _from = Settings.CalDavFrom
+        let _to = Settings.CalDavTo
+
+        let _serviceAccountId = 1
+
+        let login : Repository.Login = { userName = _userName; password = _password; server = _server.ToString(); serviceAccountId = _serviceAccountId }
+        Repository.download(_from, _to, login)
+
