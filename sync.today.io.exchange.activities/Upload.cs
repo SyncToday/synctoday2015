fc0cfac2e8706e07fb7b55dfd97730b44a85e207
davidpodhola <david.podhola@naseukoly.cz>
2015-04-23 21:19:38 +0200

    sync.today.io.activities

--- sync.today.io.exchange.activities/Upload.cs
+++ sync.today.io.exchange.activities/Upload.cs
@@ -1,4 +1,5 @@
 ﻿using sync.today.activities;
+using sync.today.io.activities;
 using System;
 using System.Activities;
 using System.Collections.Generic;
@@ -8,13 +9,11 @@ using System.Threading.Tasks;
 
 namespace sync.today.io.exchange.activities
 {
-    public sealed class Upload : BaseCodeActivity
+    public sealed class Upload : BaseUpload
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger
     (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        public InArgument<Models.ServiceAccountDTO> ServiceAccount { get; set; }
-        public InArgument<bool> Ignore { get; set; }
         protected override void DoExecute(CodeActivityContext context)
         {
             devlog.Debug(string.Format("Entered for '{0}' and '{1}'", ServiceAccount, Ignore));
