46e5572992dab8c9885fc0c30dfc2050cef5371c
David Podhola <david.podhola@naseukoly.cz>
2015-04-23 14:43:07 +0000

    autoget after merge
    git-tfs-id: [https://synctoday.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C111

--- sync.today.version.fsharp/AssemblyFileVersion.fs
+++ sync.today.version.fsharp/AssemblyFileVersion.fs
@@ -1,4 +1,4 @@
 module sync.today.AssemblyFileVersion
 #if RELEASE      
-      [<assembly:System.Reflection.AssemblyFileVersion("15.04.23.0954")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.04.23.1627")>]
 #endif
