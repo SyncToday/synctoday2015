bc39bc54e86e55d96a15284b767a80e81b0d8fbf
David Podhola <david.podhola@naseukoly.cz>
2015-04-27 23:48:04 +0000

    caldav fixed (buildable) to be part of workflow runner
    git-tfs-id: [https://synctoday.visualstudio.com/DefaultCollection]$/Sync.Today/Main/2015;C117

--- sync.today.io.caldav.activities/ConvertCalDAVAdapterAppointmentBack.cs
+++ sync.today.io.caldav.activities/ConvertCalDAVAdapterAppointmentBack.cs
@@ -10,18 +10,18 @@ namespace sync.today.io.caldav.activities
 {
     public sealed class ConvertCalDAVAdapterAppointmentBack : BaseConvertIOAdapterAppointmentBack
     {
-        public InArgument<Models.CalDAVEventDTO> OriginalCalDAVAppointment { get; set; }
-        public OutArgument<Models.CalDAVEventDTO> CalDAVAppointment { get; set; }
+        public InArgument<Models.CalDAVEventDTO> OriginalCalDAVEvent { get; set; }
+        public OutArgument<Models.CalDAVEventDTO> CalDAVEvent { get; set; }
 
         protected override void DoExecute(System.Activities.CodeActivityContext context)
         {
             devlog.Debug(string.Format("Called on '{0}'and  '{1}'", AdapterAppointment, ServiceAccountId));
             var myAdapterAppointment = AdapterAppointment.Get(context);
             var myServiceAccountId = ServiceAccountId.Get(context);
-            var myOriginalAppointment = OriginalCalDAVAppointment.Get(context);
+            var myOriginalAppointment = OriginalCalDAVEvent.Get(context);
             devlog.Debug(string.Format("Got '{0}' and  '{1}' and '{2}'", myAdapterAppointment, myServiceAccountId, myOriginalAppointment));
             var Appointment = Repository.ConvertFromDTO(myAdapterAppointment, myServiceAccountId, myOriginalAppointment);
-            CalDAVAppointment.Set(context, Appointment);
+            CalDAVEvent.Set(context, Appointment);
         }
     }
 }
