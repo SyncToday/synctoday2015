2023a4158372e7a82508cde2ae75c12271c2548f
davidpodhola <david.podhola@naseukoly.cz>
2015-05-05 21:43:20 +0200

    sync.today.searchreplace replaces travis cnn for both the localdb and kc8q88iui6.database.windows.net servers

--- tools/sync.today.searchreplace/sync.today.searchreplace/Program.fs
+++ tools/sync.today.searchreplace/sync.today.searchreplace/Program.fs
@@ -1,6 +1,8 @@
 ﻿open System.IO
 
-let oldCnn = """Server=(localdb)\ProjectsV12; Database=SyncToday2015.new; Trusted_Connection=True;"""
+let oldCnn1 = """Server=(localdb)\ProjectsV12; Database=SyncToday2015.new; Trusted_Connection=True;"""
+let oldCnn2 = """Data Source=kc8q88iui6.database.windows.net;Initial Catalog=sync.today.db.test;Integrated Security=False;User ID=sync.today;Password=Betpum57;Connect Timeout=60;Encrypt=False"""
+
 let newCnn = """Server=xjawltgq7t.database.windows.net,1433;Database=travis;User ID=travis@xjawltgq7t;Password=pkvrzSeHp86KQktm;Trusted_Connection=False;Connection Timeout=30;"""
 
 [<EntryPoint>]
@@ -8,6 +10,7 @@ let main argv =
     let files = Directory.EnumerateFiles(".", "*.config", SearchOption.AllDirectories)
     for fileName in files do
         let content = File.ReadAllText( fileName )
-        let replacedContent = content.Replace(oldCnn, newCnn)
-        File.WriteAllText(fileName, replacedContent)
+        let replacedContent1 = content.Replace(oldCnn1, newCnn)
+        let replacedContent2 = replacedContent1.Replace(oldCnn2, newCnn)
+        File.WriteAllText(fileName, replacedContent2)
     0 // return an integer exit code
