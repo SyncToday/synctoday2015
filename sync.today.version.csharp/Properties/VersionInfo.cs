3b6f9ba2a211f1a205d2e65ab5679ed2d4c3c524
davidpodhola <david.podhola@naseukoly.cz>
2015-05-15 23:17:15 +0200

    autogen

--- sync.today.version.csharp/Properties/VersionInfo.cs
+++ sync.today.version.csharp/Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
 #if RELEASE
-      [assembly:System.Reflection.AssemblyFileVersion("15.05.15.2147")]
+      [assembly:System.Reflection.AssemblyFileVersion("15.05.15.2302")]
       #endif
--- sync.today.version.fsharp/AssemblyFileVersion.fs
+++ sync.today.version.fsharp/AssemblyFileVersion.fs
@@ -1,4 +1,4 @@
 module sync.today.AssemblyFileVersion
 #if RELEASE      
-      [<assembly:System.Reflection.AssemblyFileVersion("15.05.15.2145")>]
+      [<assembly:System.Reflection.AssemblyFileVersion("15.05.15.2302")>]
 #endif
